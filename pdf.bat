b7eaf3dc871d60cd2f7f2a2c1a42102509036b26
bug400 <bug400@gmx.de>
2022-12-16 08:59:51 +0100

    Hide Node.js cmd window

--- pdf.bat
+++ pdf.bat
@@ -2,7 +2,7 @@
 ::
 :: start and stop the NOS/BE print formatter
 ::
-if /I "%1"=="start" start "nosbeformatter" /D . /B  cmd /c  node lpt2pdf\nosbeformatter.js -p LP5xx_C12_E5 -s printfiles -o "-tof 3 -require APPEND" > pdf.log 2>&1
+if /I "%1"=="start" start "nosbeformatter" /D . /min  cmd /c node lpt2pdf\nosbeformatter.js -p LP5xx_C12_E5 -s printfiles -o "-tof 3 -require APPEND" > pdf.log 2>&1
 if /I "%1"=="stop"  (
 	for /F %%p in (pdf.pid) do taskkill /pid %%p /F /T
 )
